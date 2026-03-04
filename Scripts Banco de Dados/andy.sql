84dbc4f1767eec646b22abff9609e72c748abf83
Andylinaraujo <andy.araujo@sptech.school>
2026-03-04 12:41:27 -0300

    Update andy.sql

--- Scripts Banco de Dados/andy.sql
+++ Scripts Banco de Dados/andy.sql
@@ -2,9 +2,13 @@ create database vinho;
 
 use vinho;
 
-create table login(
-id int primary key not null,
-nome varchar (100) not null,
-senha varchar (100) not null
+create table usuário(
+id int primary key not null auto_increment,
+nome varchar (30) not null,
+senha varchar (30) not null,
+cnpj char(14) not null,
+número varchar (9),
+email varchar(100),
+empresa varchar (40)
 );
-
+alter table usuário add constraint chk_senha check(nome >=8 );
