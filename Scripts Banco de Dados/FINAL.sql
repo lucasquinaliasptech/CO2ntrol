d20703b1050b82569875eef9bb296faf33d34fe6
Miguel-SpTech <miguel.mauer@sptech.school>
2026-03-06 10:44:48 -0300

    versao-final-sql

---
+++ Scripts Banco de Dados/FINAL.sql
@@ -0,0 +1,42 @@
+create database CO2ntrol;
+
+use CO2ntrol;
+
+create table usuario (
+	id int primary key auto_increment,
+    nome_fantasia varchar(100),
+    razao_social varchar(100) not null,
+    cnpj char(14) not null,
+    telefone char(11),
+    email varchar(100) not null,
+    senha varchar(100) not null,
+    situacao tinyint check (situacao in (0,1)) not null default 1
+);
+
+create table endereco (
+	id int primary key auto_increment,
+    id_usuario int not null,
+    cep char(8) not null,
+    logradouro varchar(100) not null,
+    numero int,
+    complemento varchar(100),
+    estado char(2) not null,
+    municipio varchar(50) not null
+);
+
+create table armazenamento (
+	id int primary key auto_increment,
+    id_usuario int not null,
+    tipo varchar(20) check (tipo in ('FOUDRE', 'TANQUE')) not null,
+    capacidade decimal(10000,2) not null,
+    nivel_carbono_ideal decimal(100,2) not null,
+    utilizacao tinyint check (utilizacao in (0,1)) not null
+);
+
+create table sensor (
+	id int primary key auto_increment,
+    id_armazenamento int not null,
+    nivel_carbono decimal (100,2),
+    data_registro datetime default current_timestamp(),
+    situacao tinyint check (situacao in (0, 1))
+);
