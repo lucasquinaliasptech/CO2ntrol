cc2d20b041c31c5cedbdd3a2ba45305db0169507
lucasquinaliasptech <lucas.quinalia@sptech.school>
2026-03-08 20:16:53 -0300

    Scripts banco de dados - Lucas

---
+++ Scripts Banco de Dados/inserts/inserts-lucas.sql
@@ -0,0 +1,9 @@
+insert into sensor (id_armazenamento, nivel_carbono, nivel_carbono_min, nivel_carbono_max, situacao)
+	values
+		(1, 0.80, 0.2, 0.4, 1),
+		(1, 0.70, 0.2, 0.4, 1),
+		(2, 0.20, 0.2, 0.4, 1),
+		(3, 0.30, 0.2, 0.4, 1),
+		(4, 0.20, 0.2, 0.4, 1),
+		(5, 0.60, 0.2, 0.4, 1)
+;
